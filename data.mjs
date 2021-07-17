cf6ce7629bda0f3b5a86f93af449253415bdd3cd
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-07-17 14:56:58 -0700

    add correct getItem function for HW2

--- data.mjs
+++ data.mjs
@@ -1,6 +1,6 @@
 let plantBasedMilk = [
-    { name : "Oatly", producer : "Sweden", type : "Oat milk", price : "$5" },
-    { name : "Silk Almond", producer : "US", type : "Oat milk", price : "$4" },
+    { name : "oatly", producer : "Sweden", type : "Oat milk", price : "$5" },
+    { name : "silk Almond", producer : "US", type : "Oat milk", price : "$4" },
     { name : "Pacific Hemp", producer : "US", type : "Hemp milk", price : "$4" },
     { name : "Alpro", producer : "Belgium", type : [ "Oat milk", "Soya milk" ], price : "$4" },
     { name : "So Delicious", producer : "US", type : "Cashew milk", price : "$4" }
@@ -10,11 +10,11 @@ let plantBasedMilk = [
         return { name : plantMilk.name, producer : plantMilk.producer, type : plantMilk.type, price : plantMilk.price }
     });
 
-    const getItem = plantBasedMilk.filter((plantMilk) => {
-        return plantMilk.producer === "Sweden";
-    }); 
-    
-    // console.log(getItem)
 
+    const getItem = (milkBrand) => {
+        return plantBasedMilk.find((name) => {
+            return name.name == milkBrand;
+        });
+    }
 
     export { getAll, getItem } ;
